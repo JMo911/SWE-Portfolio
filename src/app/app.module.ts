0f6a74912dab02f99e19d9c1fabf8682787027b0
Jacob Moss <jmoss@indeed.com>
2020-06-27 15:36:21 -0500

    tabs animation working as expected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+// ANGULAR MODULES
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+
+// MY COMPONENTS
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { GamesPageComponent } from './games-page/games-page.component';
 import { ContactFormComponent } from './contact-page/contact-form/contact-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
+import { SocialMediaTabsComponent } from './contact-page/social-media-tabs/social-media-tabs.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,8 @@ import { MatIconModule } from '@angular/material/icon';
     ProjectPageComponent,
     ContactPageComponent,
     GamesPageComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    SocialMediaTabsComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +35,8 @@ import { MatIconModule } from '@angular/material/icon';
     ReactiveFormsModule,
     RouterModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-page/social-media-tabs/social-media-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-media-tabs',
+  templateUrl: './social-media-tabs.component.html',
+  styleUrls: ['./social-media-tabs.component.scss']
+})
+export class SocialMediaTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
