b2a20a49d2c8f068f9277f8b9948eba526151595
Jacob Moss <jmoss@indeed.com>
2020-06-29 19:59:47 -0500

    graphql query working as expected

--- schema.js
+++ schema.js
@@ -4,37 +4,9 @@ const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt,
   GraphQLSchema,
-  GraphQLList,
-  GraphQLNonNull
+  GraphQLList
 } = require('graphql');
-
-const githubEndPoint = "https://api.github.com/graphql";
-
-// Hardcoded Data
-const projects = [
-  {
-      "name": "project3auth",
-      "description": "V-STAT solves transparency problems in the automotive repair process by connecting clients, insurance agents, and mechanics in one portal that shows insurance case information, vehicle repair tasks/status, and a comments thread.",
-      "url": "https://github.com/JMo911/project3auth"
-  },
-  {
-      "name": "PassportReactDemo",
-      "description": "ValetX revolutionizes the valet process by creating electronic tickets, handling all transactions, and allowing guests to request their car from within the app. No more paper tickets or awkward interactions with valets.",
-      "url": "https://github.com/JMo911/PassportReactDemo"
-  },
-  {
-      "name": "DiagNotes",
-      "description": "DiagNotes strives to automate the clinical documentation process and alleviate the burden so that doctors can truly be with their patients.",
-      "url": "https://github.com/JMo911/DiagNotes"
-  },
-  {
-      "name": "GoogleBooks",
-      "description": "This app provides a simple Google Books API search that allows users to save books they like and find the books on Google.",
-      "url": "https://github.com/JMo911/GoogleBooks"
-  }
-]
 
 // Project Type
 const ProjectType = new GraphQLObjectType({
@@ -46,12 +18,11 @@ const ProjectType = new GraphQLObjectType({
   })
 });
 
-// root query
-//   return axios.post(githubEndPoint, {
-//     query: repoQuery(name, repo)
-//   }, {headers: auth});
+// QUERY VARIABLES
+const githubEndPoint = "https://api.github.com/graphql";
+const auth = {Authorization: `bearer ${process.env.GITHUBTOKEN}`}
 const myQuery = `
-{
+query {
   user(login:"JMo911") {
   pinnedItems(first: 6, types: [REPOSITORY]) {
     totalCount
@@ -69,17 +40,23 @@ const myQuery = `
 }
 `
 
-const auth = {Authorization: `bearer ${process.env.GITHUBTOKEN}`}
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     projects: {
       type: new GraphQLList(ProjectType),
-      resolve(parentValue) {
+      resolve() {
         return axios.post(githubEndPoint, {
           query: myQuery
         }, {headers: auth})
-        .then(res => res.data);
+        .then(res => {
+          const initialArray = res.data.data.user.pinnedItems.edges;
+          const projectsArray = [];
+          initialArray.forEach(element => {
+            projectsArray.push(element.node);
+          });
+          return projectsArray;
+        });
       }
     }
   }
@@ -88,36 +65,3 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
   query: RootQuery
 });
-
-// require('dotenv').config();
-// const axios = require('axios');
-
-// const auth = {Authorization: `bearer ${process.env.GITHUBTOKEN}`}
-// const githubEndPoint = "https://api.github.com/graphql";
-
-// function repoQuery(name, repo) {
-//   return `
-//     {
-//       user(login:"JMo911") {
-//         pinnedItems(first: 6, types: [REPOSITORY]) {
-//           totalCount
-//           edges {
-//             node {
-//               ... on Repository {
-//                 name
-//                 description
-//                 url
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `
-// }
-
-// function fetchRepositoryData(name, repo) {
-//   return axios.post(githubEndPoint, {
-//     query: repoQuery(name, repo)
-//   }, {headers: auth});
-// }
