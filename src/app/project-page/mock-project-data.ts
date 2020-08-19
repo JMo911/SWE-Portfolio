f20d64f44c186dd08bb4108d44c348850e864276
Jacob Moss <jmoss@indeed.com>
2020-08-18 19:10:52 -0500

    project-page running with mock data

---
+++ src/app/project-page/mock-project-data.ts
@@ -0,0 +1,39 @@
+export const mockProjects = {
+  "data": {
+    "user": {
+      "pinnedItems": {
+        "totalCount": 4,
+        "edges": [
+          {
+            "node": {
+              "name": "project3auth",
+              "description": "V-STAT solves transparency problems in the automotive repair process by connecting clients, insurance agents, and mechanics in one portal that shows insurance case information, vehicle repair tasks/status, and a comments thread.",
+              "url": "https://github.com/JMo911/project3auth"
+            }
+          },
+          {
+            "node": {
+              "name": "PassportReactDemo",
+              "description": "ValetX revolutionizes the valet process by creating electronic tickets, handling all transactions, and allowing guests to request their car from within the app. No more paper tickets or awkward interactions with valets.",
+              "url": "https://github.com/JMo911/PassportReactDemo"
+            }
+          },
+          {
+            "node": {
+              "name": "DiagNotes",
+              "description": "DiagNotes strives to automate the clinical documentation process and alleviate the burden so that doctors can truly be with their patients.",
+              "url": "https://github.com/JMo911/DiagNotes"
+            }
+          },
+          {
+            "node": {
+              "name": "GoogleBooks",
+              "description": "This app provides a simple Google Books API search that allows users to save books they like and find the books on Google.",
+              "url": "https://github.com/JMo911/GoogleBooks"
+            }
+          }
+        ]
+      }
+    }
+  }
+}
